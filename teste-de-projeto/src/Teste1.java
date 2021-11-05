4c0c8010683061f3264c0986743a249ca5caa589
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2021-11-04 22:58:24 -0300

    comit to main

---
+++ teste-de-projeto/src/Teste1.java
@@ -0,0 +1,12 @@
+
+public class Teste1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ola mundo 1");
+		System.out.println("Ola mundo 2");
+		System.out.println("Ola mundo 3");
+	}
+
+}
